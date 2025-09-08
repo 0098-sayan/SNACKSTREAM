02dc1c9bb29db14bf2423bdf198b6c892d05751b
0098-sayan <maitysayan704@gmail.com>
2025-09-08 22:47:33 +0530

    Adding tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import react from "react";
-
-import "./App.css";
+import React from 'react'
 
 function App() {
-  return;
-  <>
-
-  </>;
+  return (
+    <div>
+      <h1 className='bg-red-600 font-bold uppercase text-5xl flex items-center justify-center'>Hello world</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
